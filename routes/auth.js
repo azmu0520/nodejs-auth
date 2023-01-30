2c2f6c556a32bbe06aeb13093519f698b73cab0d
azmu0520 <azizbekbootcam@gmail.com>
2023-01-30 13:22:55 +0500

    done

--- routes/auth.js
+++ routes/auth.js
@@ -11,6 +11,7 @@ router.post('/register', async (req, res) => {
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
+    status: true,
   });
   try {
     const savedUser = await user.save();
